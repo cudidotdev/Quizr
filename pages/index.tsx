3dca1975e634bfc1c2eafde420cbc790c2d45104
CudiLala <augustinemadu9@gmail.com>
2022-03-31 14:25:11 +0100

    fixed deploy error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPageWithLayout } from "types/next";
-import styles from "styles/pages/Home.module.css";
 import Layout from "components/layouts";
 
 const Home: NextPageWithLayout = () => {
