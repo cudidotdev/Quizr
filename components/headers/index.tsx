bef9fc8b694ad3a5399570179dcf9e39b95be9e8
CudiLala <augustinemadu9@gmail.com>
2022-04-01 01:23:09 +0100

    test

--- components/headers/index.tsx
+++ components/headers/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { HelpIcon, Logo, PodiumIcon, UserIcon } from "components/icon";
 import styles from "styles/components/headers.module.css";
 import { Linkr } from "components/links";
 
 const Header: React.FC = () => {
+  const [width, setWidth] = useState<number>(0);
+
+  function updateWidth() {
+    setWidth(window.innerWidth);
+  }
+
+  useLayoutEffect(() => {
+    updateWidth();
+    window.addEventListener("resize", updateWidth);
+  }, []);
+
   return (
     <header className={styles.Header}>
       <nav className={`site-width ${styles.Padder}`}>
@@ -16,20 +27,22 @@ const Header: React.FC = () => {
             Quizr
           </div>
         </Linkr>
-        <div className={styles.NavOne}>
-          <Linkr _className={`${styles.IconTextContainer}`}>
-            <PodiumIcon />
-            <div className={`${styles.IconText}`}>LeaderBoards</div>
-          </Linkr>
-          <Linkr _className={`${styles.IconTextContainer}`}>
-            <HelpIcon />
-            <div className={`${styles.IconText}`}>Help</div>
-          </Linkr>
-          <Linkr _className={`${styles.IconTextContainer}`}>
-            <UserIcon />
-            <div className={`${styles.IconText}`}>Login</div>
-          </Linkr>
-        </div>
+        {width > 600 && (
+          <div>
+            <Linkr _className={`${styles.IconTextContainer}`}>
+              <PodiumIcon />
+              <div className={`${styles.IconText}`}>LeaderBoards</div>
+            </Linkr>
+            <Linkr _className={`${styles.IconTextContainer}`}>
+              <HelpIcon />
+              <div className={`${styles.IconText}`}>Help</div>
+            </Linkr>
+            <Linkr _className={`${styles.IconTextContainer}`}>
+              <UserIcon />
+              <div className={`${styles.IconText}`}>Login</div>
+            </Linkr>
+          </div>
+        )}
       </nav>
     </header>
   );
