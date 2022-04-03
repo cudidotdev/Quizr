9c5149f58e821ac2b3e0a6c24247f6751d539e91
CudiLala <augustinemadu9@gmail.com>
2022-04-03 08:19:17 +0100

    test

--- components/texts/index.tsx
+++ components/texts/index.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import styles from "styles/components/texts.module.css";
 
 export const TextBlock: React.FC = ({ children }) => {
-  return <div className={styles.TextBlock}>{children}</div>;
+  return <p className={styles.TextBlock}>{children}</p>;
 };
