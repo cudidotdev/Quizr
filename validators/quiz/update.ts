069672d22a652dca801145114b4e0fcc4e735e7e
CudiLala <augustinemadu9@gmail.com>
2022-04-08 00:48:11 +0100

    updates on the update quiz drafts api

--- validators/quiz/update.ts
+++ validators/quiz/update.ts
@@ -69,76 +69,88 @@ function validateQuestions(questions: Array<any>, final: any) {
 
   const indexBox: number[] = [];
 
-  questions.forEach((questionObj) => {
-    const { question, index, options } = questionObj;
+  questions = questions.map((questionObj) => {
+    let { question, index, options, answer } = questionObj;
 
     if (question) {
       if (typeof question !== "string")
-        if (question.length > 256)
-          throw new ApiError(
-            "questions",
-            "Each question should be less than 256 characters",
-            400
-          );
+        throw new ApiError("question", "The question should be a string");
+
+      question = question.trim();
+
+      if (question.length > 256)
+        throw new ApiError(
+          "questions",
+          "Each question should be less than 256 characters",
+          400
+        );
     }
 
-    if (indexBox.includes(questionObj.index))
-      throw new ApiError(
-        "questions",
-        "The question index is already taken",
-        400
-      );
-    indexBox.push(questionObj.index);
+    if (index) {
+      if (!(index > 0) || !(index < 11) || !Number.isInteger(index))
+        throw new ApiError(
+          "questions",
+          "The question index should be integers from 1 to 10"
+        );
 
-    if (
-      !(questionObj.index > 0) ||
-      !(questionObj.index < 11) ||
-      !Number.isInteger(questionObj.index)
-    )
-      throw new ApiError(
-        "questions",
-        "The question index should be integers from 1 to 10"
-      );
+      if (indexBox.includes(index))
+        throw new ApiError(
+          "questions",
+          "The question index is already taken",
+          400
+        );
+      indexBox.push(index);
+    }
 
     const validOptions = ["A", "B", "C", "D"];
     const recievedOptions: string[] = [];
 
-    for (let option in questionObj.options) {
-      if (!validOptions.includes(option))
-        throw new ApiError(
-          "question",
-          "The option object should have keys in form A, B, C and D",
-          400
-        );
+    if (options) {
+      for (let option in options) {
+        if (!validOptions.includes(option))
+          throw new ApiError(
+            "question",
+            "The option object should have keys in form A, B, C and D",
+            400
+          );
 
-      if (!questionObj.options[option])
-        throw new ApiError(
-          "questions",
-          `Please include an option in ${option}`,
-          400
-        );
+        if (!options[option])
+          throw new ApiError(
+            "questions",
+            `Please include an option in ${option}`,
+            400
+          );
 
-      if (questionObj.options[option].length > 256)
-        throw new ApiError(
-          "questions",
-          `Each option should contain at most 256 characters`,
-          400
-        );
+        if (typeof options[option] !== "string")
+          throw new ApiError("questions", "All options should be strings", 400);
 
-      recievedOptions.push(option);
+        options[option] = options[option].trim();
+
+        if (options[option].length > 256)
+          throw new ApiError(
+            "questions",
+            `Each option should contain at most 256 characters`,
+            400
+          );
+
+        recievedOptions.push(option);
+      }
+
+      validOptions.forEach((opt) => {
+        if (!recievedOptions.includes(opt))
+          throw new ApiError("questions", `Please include option ${opt}`, 400);
+      });
     }
 
-    validOptions.forEach((opt) => {
-      if (!recievedOptions.includes(opt))
-        throw new ApiError("questions", `Please include option ${opt}`, 400);
-    });
+    if (answer)
+      if (!validOptions.includes(questionObj.answer))
+        throw new ApiError(
+          "questions",
+          "The answer should be in form A, B, C or D",
+          400
+        );
 
-    if (!validOptions.includes(questionObj.answer))
-      throw new ApiError(
-        "questions",
-        "The answer should be in form A, B, C or D",
-        400
-      );
+    return { question, index, options, answer };
   });
 
   final.questions = questions;
