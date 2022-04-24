ac9a506900391b53645d3cdb6d09db339e074833
CudiLala <augustinemadu9@gmail.com>
2022-04-24 16:42:40 +0100

    minor change to Admin.module.css

--- page_components/index/quiz_list_app/index.tsx
+++ page_components/index/quiz_list_app/index.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import ListContainer, { LinkList } from "components/lists";
+import React, { useEffect, useState } from "react";
+import { getFetcher } from "utils/fetchers";
 import QuizSortForm from "./quiz_sort_form";
 
-const QuizListApp: React.FC = () => (
-  <div className="pad-bottom-one">
-    <QuizSortForm />
-  </div>
-);
+const QuizListApp: React.FC = () => {
+  const [quizzes, setQuizzes] = useState<any[]>([]);
 
+  async function fetchQuizzes() {
+    const res = await getFetcher("/api/quiz");
+    const { success, data, error } = res;
+
+    setQuizzes(data);
+  }
+
+  useEffect(() => {
+    fetchQuizzes();
+  }, []);
+
+  return (
+    <div className="pad-bottom-one">
+      <QuizSortForm />
+      <div style={{ padding: "0.75rem 0" }} className="content-width">
+        <ListContainer>
+          {quizzes.map((quiz) => (
+            <LinkList key={quiz._id}>{quiz.title}</LinkList>
+          ))}
+        </ListContainer>
+      </div>
+    </div>
+  );
+};
 export default QuizListApp;
