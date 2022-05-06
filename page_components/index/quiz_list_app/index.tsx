fe7825972ccd37316e97064564269ec893753ac2
CudiLala <augustinemadu9@gmail.com>
2022-05-06 10:17:31 +0100

    creating branch for homepage quiz loading component

--- page_components/index/quiz_list_app/index.tsx
+++ page_components/index/quiz_list_app/index.tsx
@@ -1,12 +1,7 @@
-import ListContainer, { LinkList } from "components/lists";
 import React, { useEffect, useState } from "react";
 import { getFetcher } from "utils/fetchers";
 import QuizSortForm from "./quiz_sort_form";
-import styles from "styles/pages/Home.module.css";
-import aStyles from "styles/pages/Admin.module.css";
-import { TripleSquareLoader } from "components/loaders";
-import { ReloadIcon } from "components/icons";
-import { Text } from "components/texts";
+import QuizList from "./quiz_list";
 
 const QuizListApp: React.FC = () => {
   const [quizzes, setQuizzes] = useState<any[]>([]);
@@ -38,37 +33,12 @@ const QuizListApp: React.FC = () => {
   return (
     <div className="pad-bottom-one">
       <QuizSortForm />
-      <div style={{ padding: "0.75rem 0" }} className="content-width">
-        <Text className={`${styles.QuizListHeading} t-medium`}>Quizzes</Text>
-        {loading ? (
-          <div className={aStyles.MsgContainer}>
-            <p className={aStyles.LoaderBox}>
-              Fetching quizzes
-              <TripleSquareLoader colored />
-            </p>
-          </div>
-        ) : quizLoadError.val ? (
-          <div className={aStyles.MsgContainer}>
-            <p className={aStyles.ErrorBox}>
-              Couldn&apos;t fetch quizzes: {quizLoadError.msg}
-            </p>
-            <button className={aStyles.ReloadButton} onClick={fetchQuizzes}>
-              Reload
-              <span className={aStyles.Icon}>
-                <ReloadIcon />
-              </span>
-            </button>
-          </div>
-        ) : (
-          <ListContainer>
-            {quizzes.map((quiz) => (
-              <LinkList key={quiz._id} href={`/q/${quiz.urlName}`}>
-                {quiz.title}
-              </LinkList>
-            ))}
-          </ListContainer>
-        )}
-      </div>
+      <QuizList
+        loading={loading}
+        quizLoadError={quizLoadError}
+        fetchQuizzes={fetchQuizzes}
+        quizzes={quizzes}
+      />
     </div>
   );
 };
--- page_components/index/quiz_list_app/quiz_list/index.tsx
+++ page_components/index/quiz_list_app/quiz_list/index.tsx
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ListContainer, { LinkList } from "components/lists";
+import { getFetcher } from "utils/fetchers";
+import styles from "styles/pages/Home.module.css";
+import aStyles from "styles/pages/Admin.module.css";
+import { TripleSquareLoader } from "components/loaders";
+import { ReloadIcon } from "components/icons";
+import { Text } from "components/texts";
 
-const QuizList: React.FC = () => <></>;
+const QuizList: React.FC<quizListProps> = ({
+  loading,
+  quizLoadError,
+  fetchQuizzes,
+  quizzes,
+}) => {
+  return (
+    <div style={{ padding: "0.75rem 0" }} className="content-width">
+      <Text className={`${styles.QuizListHeading} t-medium`}>Quizzes</Text>
+      {loading ? (
+        <div className={aStyles.MsgContainer}>
+          <p className={aStyles.LoaderBox}>
+            Fetching quizzes
+            <TripleSquareLoader colored />
+          </p>
+        </div>
+      ) : quizLoadError.val ? (
+        <div className={aStyles.MsgContainer}>
+          <p className={aStyles.ErrorBox}>
+            Couldn&apos;t fetch quizzes: {quizLoadError.msg}
+          </p>
+          <button className={aStyles.ReloadButton} onClick={fetchQuizzes}>
+            Reload
+            <span className={aStyles.Icon}>
+              <ReloadIcon />
+            </span>
+          </button>
+        </div>
+      ) : (
+        <ListContainer>
+          {quizzes.map((quiz) => (
+            <LinkList key={quiz._id} href={`/q/${quiz.urlName}`}>
+              {quiz.title}
+            </LinkList>
+          ))}
+        </ListContainer>
+      )}
+    </div>
+  );
+};
+
+type quizListProps = {
+  loading: boolean;
+  quizLoadError: { val: boolean; msg: string };
+  fetchQuizzes: () => any;
+  quizzes: any[];
+};
 
 export default QuizList;
