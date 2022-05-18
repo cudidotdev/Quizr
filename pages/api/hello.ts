cee357e309c2e6a45539ffeab6306d291f851662
CudiLala <augustinemadu9@gmail.com>
2022-05-18 16:29:17 +0100

    modified quiz fetching api to include search

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { restrictToLogin } from "api_middlewares";
 import connectDB from "database/connect";
-import { quizSearchIndex, User } from "database/models";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NextApiHandlerX } from "types/next";
 
